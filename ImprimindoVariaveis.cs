450595fc33b5e8b3a8550852993490ea13eaa3a2
Everlan de Souza <everlansoouza@gmail.com>
2021-06-03 21:19:55 -0300

    Conteudo da Aula 2 completo

---
+++ ImprimindoVariaveis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course2 {
+    class ImprimindoVariaveis {
+        static void Main(string[] args) {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.357533;
+            string nome = "Maria";
+
+
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!!");
+            Console.WriteLine("Boa noite!!!");
+
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+        }
+    }
+}
